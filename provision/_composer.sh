aac5244f317ab2935183b9a45f09b9653fac8584
wansoft <fabrizio@wansoft.it>
2015-01-29 16:34:10 +0100

    fixed _composer.sh

--- provision/_composer.sh
+++ provision/_composer.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+
 cd ${PROJECT_DIR}
 
 # composer -n init --name="myvendor/mypackage" --description="my project"  --author="My Name <myname@amyvendor.com>"   --require="magento-hackathon/magento-composer-installer:*" --license="proprietary"
 # --require="magento/core:${MAGENTO_VERSION}"
 
-cp provision/composer.json .
+cp ${PROVISION_DIR}/composer.json .
 
 composer --no-interaction install
 composer --no-interaction update
